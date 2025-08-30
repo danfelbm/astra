d63501ebca63c589e290ba8dd36625f20b80e13c
danfelbm <danfelbm@gmail.com>
2025-08-30 17:10:56 -0500

    fix: Permite edición de votaciones activas mientras se mantenga flexibilidad administrativa

---
+++ public/build/assets/Skeleton.vue_vue_type_script_setup_true_lang-TBVYMMqK.js
@@ -0,0 +1,1 @@
+import{a}from"./createLucideIcon-CdhR9dO9.js";import{d as n,i as o,e as r,n as t,u as c}from"./app-Cq1yjciB.js";const i=n({__name:"Skeleton",props:{class:{}},setup(e){const s=e;return(l,m)=>(r(),o("div",{class:t(c(a)("animate-pulse rounded-md bg-muted",s.class))},null,2))}});export{i as _};
