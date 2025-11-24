a23657f5a547cad5208bc34f7bdf9b25fb73bc19
danfelbm <danfelbm@gmail.com>
2025-11-23 22:50:36 -0500

    refactor(obligaciones): Unificar tab Actividad con componentes reutilizables
    Backend (ObligacionContratoController.php):
    - Agregar extracción de usuariosActividades desde las actividades
    - Pasar usuariosActividades a Inertia para los filtros
    
    Frontend (Obligaciones/Show.vue):
    - Importar componentes ActivityFilters y ActivityLog
    - Agregar prop usuariosActividades al interface Props
    - Crear estado filtrosActividades ref
    - Implementar computed actividadesFiltradas con lógica de filtrado
    - Reemplazar código hardcoded del tab Actividad con componentes reutilizables
    - Eliminar ~30 líneas de código duplicado
    
    Ahora Obligaciones usa los mismos componentes que Proyectos, Hitos y Entregables,
    manteniendo consistencia en la UI y eliminando duplicación de código.

--- public/build/assets/CollapsibleTrigger.vue_vue_type_script_setup_true_lang-BV7oG5Rw.js
+++
@@ -1,1 +0,0 @@
-import{Y as o}from"./index-CUIGqQ1J.js";import{d as r,b as t,w as n,Z as p,aa as l,ab as i,f as c,m}from"./app-DB0z6AcD.js";const _=r({__name:"CollapsibleTrigger",props:{asChild:{type:Boolean},as:{}},setup(a){const e=a;return(s,f)=>(m(),t(c(o),l(i(e)),{default:n(()=>[p(s.$slots,"default")]),_:3},16))}});export{_};
