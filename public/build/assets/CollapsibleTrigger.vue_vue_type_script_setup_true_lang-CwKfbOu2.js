9edc1b45bbbe2391cae8bf94a3a4bf8385ef55b0
danfelbm <danfelbm@gmail.com>
2025-11-25 15:35:47 -0500

    feat(obligaciones): Usar modal selector de contratos en filtros del Index
    - Reemplazar dropdown por AddContratosModal en ObligacionFilters
    - Agregar chip visual para mostrar contrato activo desde URL con botón para quitar
    - Pasar proyectos en lugar de contratos desde el controlador

---
+++ public/build/assets/CollapsibleTrigger.vue_vue_type_script_setup_true_lang-CwKfbOu2.js
@@ -0,0 +1,1 @@
+import{Y as o}from"./index-DAR7lzPI.js";import{d as r,b as t,w as n,Z as p,aa as l,ab as i,f as c,m}from"./app-DsXE2pWc.js";const _=r({__name:"CollapsibleTrigger",props:{asChild:{type:Boolean},as:{}},setup(a){const e=a;return(s,f)=>(m(),t(c(o),l(i(e)),{default:n(()=>[p(s.$slots,"default")]),_:3},16))}});export{_};
