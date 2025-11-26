53815eb1ce495485424c7b368c7e2917177279b3
danfelbm <danfelbm@gmail.com>
2025-11-26 17:13:19 -0500

    style(hitos): Ajustar grid a 2 columnas y actualizar build assets
    - Cambiar vista de hitos en grid de 3 a 2 columnas en pantallas grandes
    - Actualizar assets de build compilados

--- public/build/assets/CardFooter.vue_vue_type_script_setup_true_lang-_fBgKwz7.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./createLucideIcon-DHJS65Rc.js";import{d as r,i as t,n,f as c,M as l,m as p}from"./app-LQhORHQe.js";const d=r({__name:"CardFooter",props:{class:{}},setup(s){const e=s;return(a,m)=>(p(),t("div",{class:n(c(o)("flex items-center p-6 pt-0",e.class))},[l(a.$slots,"default")],2))}});export{d as _};
